7b5d916308e80f0c0d2258b5642d937dc267e3a2
Bodil-T <s173872@student.dtu.dk>
2022-04-26 09:50:26 +0200

    parameters and initial eq
    New parameters and the beginning og the copepods movement

--- test.m
+++ test.m
@@ -5,6 +5,7 @@ clc
 
 "Running"
 
+% hello git
 %% 1) Parameters:
 p.u= 0.96; %sinking speed [m/day]
 p.D= 10;%diffusion rate [m^2/day] 
@@ -25,6 +26,15 @@ p.Hn=0.04; %Half satuaration nutrients growth [mmol N/(m^3)]
 
 p.tau=0.1; %1/d  remineralization
 p.w=5; %sinking speed of detritus m/d
+
+% New parameters
+p.r=1/3
+p.c=0.5
+p.b=0.5
+p.h=0.5
+p.kl=0.5
+
+p.DeltaT=1
 
 
 %2) tThe grid
@@ -47,7 +57,10 @@ y=[P0,N0,D0];
 
 %% RUN ODE
 % 4) Set time step !!!
-t1=[0:3*365];
+
+tt=3*365
+
+t1=[0:tt];
 
 %4) Run the model
 [t,y]=ode45(@func_diff,t1,y,[],p);
@@ -65,6 +78,43 @@ I=func_light(z,Ps(end,:),p);
 %% AGENT BASED MODEL
 % Copepods
 
+g0=p.b*Ps(t,z(t))/(1+p.b*p.h*z(t)
+m0=p.kl*I(t+1,z(t))
+w0=g0/m0
+dwdz=
+Z(1)=-30;
 
+for i=1:tt
+Z(i+1)=Z(i)+(-1+2*rand())*(2*p.r^(-1)*p.D*p.DeltaT)^(1/2)+p.c*dwdz*p.DeltaT;
+if Z(i+1)>0;
+    Z(i+1)=0;
+end
+if Z(i+1)<100;
+    Z(i+1)=100;
+end
+end
 
+%+p.c*dwdz*p.DeltaT
 
+%g=p.b*Ps(t+1,z(t+1))/(1+p.b*p.h*z(t+1)
+
+%m=p.kl*I(t+1,z(t+1))
+
+%w=g/m 
+
+%% 5) plot Plankton
+figure() %plot
+contourf(t,-z,Ps')
+c=colorbar;
+c.Label.String = 'Concentration of PP [cells/m^3]';
+hold on
+plot(t,Z','Color','#A2142F','Linewidth',1)
+hold off
+ylabel("Depth [m]")
+xlabel("Time [days]")
+title("Distribution of Phytoplankton")
+grid on
+
+%%
+
+"done"
